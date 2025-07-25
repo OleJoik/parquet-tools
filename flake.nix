9ba436b2d69c20d7bf171eb7486ff66195242ccc
OleJoik <57186239+OleJoik@users.noreply.github.com>
2025-07-25 11:48:05 +0200

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Apache Parquet Tools flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      parquetToolsJar = pkgs.fetchurl {
+        url = "https://repo1.maven.org/maven2/org/apache/parquet/parquet-tools/1.11.2/parquet-tools-1.11.2.jar";
+        sha256 = "sha256-wMrG/ALYayOqsYX2HGK/onPh/8MqkdA8o7GUSfqq1ZM="; # Verified
+      };
+
+    in pkgs.stdenv.mkDerivation {
+      name = "parquet-tools";
+      buildInputs = [ pkgs.hadoop pkgs.makeWrapper ];
+
+      dontUnpack = true;
+
+      installPhase = ''
+        mkdir -p $out/bin $out/share
+        cp ${parquetToolsJar} $out/share/parquet-tools.jar
+
+        makeWrapper ${pkgs.hadoop}/bin/hadoop $out/bin/parquet-tools \
+          --add-flags "jar $out/share/parquet-tools.jar"
+      '';
+    };
+
+    apps.x86_64-linux.default = {
+      type = "app";
+      program = "${self.packages.x86_64-linux.default}/bin/parquet-tools";
+    };
+  };
+}
